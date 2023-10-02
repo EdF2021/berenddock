cc8029ee4303120f0579c5c82b9d0b5b7db19459
Ed de Feber <84625097+EdF2021@users.noreply.github.com>
2023-10-02 16:40:16 +0200

    Update ui.py

--- core/ui.py
+++ core/ui.py
@@ -1,4 +1,3 @@
-import os
 from typing import List
 import streamlit as st
 from langchain.docstore.document import Document
@@ -9,7 +8,7 @@ from typing import NoReturn
 
 logger = get_logger(__name__)
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 def wrap_doc_in_html(docs: List[Document]) -> str:
     """Wraps each page in document separated by newlines in <p> tags"""
     text = [doc.page_content for doc in docs]
